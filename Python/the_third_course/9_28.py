81944181b7123e694f8e49d509327b1e910ae3f4
quanxiao <1940325681@qq.com>
2022-10-02 16:49:15 +0800

    10.2

--- Python/the_third_course/9_28.py
+++ Python/the_third_course/9_28.py
@@ -7,35 +7,46 @@ data=np.array(df)
 
 learning_rate=0.0001
 m_train=3000
+m_test=np.shape(data)[0]-3000
 iteration=20
+epochs=1
+rng=np.random.default_rng(1)
 m_all=data.shape[0]
 d=data.shape[1]-1
 x_train=data[:m_train,1:].T
 x_test=data[m_train:,1:].T
 y_train=data[0:m_train,0].T
 y_test=data[m_train:,0].T
-w=np.zeros((d,1)).reshape(4,1)
+w=np.zeros((d,1)).reshape(-1,1)
 b=0
 
+train_set=data[0:m_train,:]
 
-v=np.ones((m_train,1)).reshape(m_train,1)
 cost_saved=[]
 
-for i in range(iteration):
-    y_hat=np.dot(w.T,x_train)+b
-    e=y_hat-y_train
-    w=w-2*learning_rate*np.dot(x_train,e.T)/m_train
-    b=b-2*learning_rate*np.dot(e,v)/m_train
-    cost=np.dot(e,e.T).item(0)/m_train
-    cost_saved.append(cost)
+for epoch in range(0,epochs):
+    rng.shuffle(train_set)
+    for i in range(m_train):
+        x=train_set[i,1:].reshape(-1,1)
+        y=train_set[i,0]
+        y_hat=np.dot(w.T,x)+b
+        e=y_hat-y
+        w=w-2*x*e*learning_rate
+        b=b-2*learning_rate*e
+        cost=e**2
+        cost_saved.append(cost.item(0))
 
-y_hat_train=np.dot(w.T,x_train)+b*v.T
+y_hat_train=np.dot(w.T,x_train)+b
 e=y_hat_train-y_train
+mse_train=np.dot(e,e.T)/m_train
+print(np.sqrt(mse_train))
+
+y_hat_test=np.dot(w.T,x_test)+b
+e=y_hat_test-y_test
+mse_test=np.sqrt(np.dot(e,e.T)/m_test)
+print(mse_test)
 
 
-y_hat_train=np.dot(w.T,x_test)+b
-e=y_hat_train-y_test
-print(np.sqrt(np.dot(e,e.T).item(0)/m_train))
-plt.plot(range(1,iteration+1),cost_saved)
+plt.plot(range(1,np.size(cost_saved)+1),cost_saved)
 plt.show()
 
--- Python/the_third_course/test.py
+++ Python/the_third_course/test.py
@@ -1,8 +1,3 @@
-from operator import truediv
-from re import A, M
-from tkinter.messagebox import YES
-from turtle import shape
 import numpy as np
-w=np.arange(4).reshape(4,-1)
-b=np.arange(4).reshape(1,4)
-k=np.dot(b,w)
+rng=np.random.default_rng()
+print(rng.random())
